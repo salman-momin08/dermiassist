d9d8eb9f1d1f03738bcfdcff155557cd1adac25c
Khwajamainuddin Mubarak Momin <khwajamainuddinmomin@gmail.com>
2026-02-22 20:51:38 +0530

    Fix Novu build-time crash

--- src/lib/novu/client.ts
+++ src/lib/novu/client.ts
@@ -1,9 +1,7 @@
 import { Novu } from '@novu/node';
 
-if (!process.env.NOVU_API_SECRET) {
-    throw new Error('Missing NOVU_API_SECRET environment variable');
-}
+const apiKey = process.env.NOVU_API_SECRET || '';
 
-export const novu = new Novu(process.env.NOVU_API_SECRET, {
+export const novu = new Novu(apiKey, {
     backendUrl: process.env.NEXT_PUBLIC_NOVU_BACKEND_URL,
 });
